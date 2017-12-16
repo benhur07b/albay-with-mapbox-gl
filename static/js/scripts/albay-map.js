c3760f2bad9229b6002bacce031fad0d3fc2cfd5
benhur07b <benhur07b@users.noreply.github.com>
2017-12-16 17:18:49 +0800

    Initial commit.

--- static/js/scripts/albay-map.js
+++ static/js/scripts/albay-map.js
@@ -20,58 +20,6 @@ map.on('load', function() {
         data: 'static/data/GRID-HEX-ALL.geojson'
     });
 
-    // map.addLayer({
-    //     'id': 'albay-cells-3d',
-    //     'type': 'fill-extrusion',
-    //     'source': 'albay-source',
-    //     'paint': {
-    //         'fill-extrusion-color': {
-    //             property: 'VAL',
-    //             stops: [
-    //                 [BREAKS[0], COLORS[0]],
-    //                 [BREAKS[1], COLORS[1]],
-    //                 [BREAKS[2], COLORS[2]],
-    //                 [BREAKS[3], COLORS[3]]]
-    //             },
-    //             'fill-extrusion-height': {
-    //                 'property': 'Elev x 3',
-    //                 'type': 'identity'
-    //             },
-    //             'fill-extrusion-opacity': 0.6
-    //         }
-    // });
-    //
-    // map.addLayer({
-    //     'id': 'albay-cells-3d-hover',
-    //     'type': 'fill-extrusion',
-    //     'source': 'albay-source',
-    //     'paint': {
-    //         'fill-extrusion-color': {
-    //             property: 'VAL',
-    //             stops: [
-    //                 [BREAKS[0], COLORS[0]],
-    //                 [BREAKS[1], COLORS[1]],
-    //                 [BREAKS[2], COLORS[2]],
-    //                 [BREAKS[3], COLORS[3]]]
-    //             },
-    //             'fill-extrusion-height': {
-    //                 'property': 'Elev x 3',
-    //                 'type': 'identity'
-    //             },
-    //             'fill-extrusion-opacity': 1.0
-    //         },
-    //     'filter': ['==', 'VAL', '']
-    // });
-    //
-    // map.on("mousemove", "albay-cells-3d", function(e) {
-    //     map.setFilter("albay-cells-3d-hover", ["==", "CID", e.features[0].properties.CID]);
-    // });
-    //
-    // // Reset the state-fills-hover layer's filter when the mouse leaves the layer.
-    // map.on("mouseleave", "albay-cells-3d", function() {
-    //     map.setFilter("albay-cells-3d-hover", ["==", "name", ""]);
-    // });
-
     map.addLayer({
         'id': 'albay-cells',
         'type': 'fill',
@@ -118,49 +66,116 @@ map.on('load', function() {
             }
     });
 
-    map.on("mousemove", "albay-cells", function(e) {
-        map.setFilter("albay-cells-hover", ["==", "CID", e.features[0].properties.CID]);
-    });
+    // map.on("mousemove", "albay-cells", function(e) {
+    //     map.setFilter("albay-cells-hover", ["==", "CID", e.features[0].properties.CID]);
+    // });
 
     // Reset the state-fills-hover layer's filter when the mouse leaves the layer.
-    map.on("mouseleave", "albay-cells", function() {
-        map.setFilter("albay-cells-hover", ["==", "name", ""]);
+    // map.on("mouseleave", "albay-cells", function() {
+    //     map.setFilter("albay-cells-hover", ["==", "name", ""]);
+    // });
+
+    map.addLayer({
+        'id': 'albay-cells-3d',
+        'type': 'fill-extrusion',
+        'source': 'albay-source',
+        'layout': {
+            'visibility': 'none'
+        },
+        'paint': {
+            'fill-extrusion-color': {
+                property: 'VAL',
+                stops: [
+                    [BREAKS[0], COLORS[0]],
+                    [BREAKS[1], COLORS[1]],
+                    [BREAKS[2], COLORS[2]],
+                    [BREAKS[3], COLORS[3]]]
+                },
+                'fill-extrusion-height': {
+                    'property': 'Elev x 3',
+                    'type': 'identity'
+                },
+                'fill-extrusion-opacity': 0.6
+            },
     });
+
+    map.addLayer({
+        'id': 'albay-cells-3d-hover',
+        'type': 'fill-extrusion',
+        'source': 'albay-source',
+        'layout': {
+            'visibility': 'none'
+        },
+        'paint': {
+            'fill-extrusion-color': {
+                property: 'VAL',
+                stops: [
+                    [BREAKS[0], COLORS[0]],
+                    [BREAKS[1], COLORS[1]],
+                    [BREAKS[2], COLORS[2]],
+                    [BREAKS[3], COLORS[3]]]
+                },
+                'fill-extrusion-height': {
+                    'property': 'Elev x 3',
+                    'type': 'identity'
+                },
+                'fill-extrusion-opacity': 1.0
+            },
+        'filter': ['==', 'VAL', '']
+    });
+    //
+    // map.on("mousemove", "albay-cells-3d", function(e) {
+    //     map.setFilter("albay-cells-3d-hover", ["==", "CID", e.features[0].properties.CID]);
+    // });
+    //
+    // // Reset the state-fills-hover layer's filter when the mouse leaves the layer.
+    // map.on("mouseleave", "albay-cells-3d", function() {
+    //     map.setFilter("albay-cells-3d-hover", ["==", "name", ""]);
+    // });
 
     // Legends
     var layers = ['↑ NDVI and ↑ LST', '↓ NDVI and ↑ LST', '↓ NDVI and LST ↑', '↓ DNVI and ↓ LST'];
     var colors = ['yellow', 'red', 'cyan', 'magenta'];
 
     for (i = 0; i < layers.length; i++) {
-      var layer = layers[i];
-      var color = colors[i];
-      var item = document.createElement('div');
-      var key = document.createElement('span');
-      key.className = 'legend-key';
-      key.style.backgroundColor = color;
+        var layer = layers[i];
+        var color = colors[i];
+        var item = document.createElement('div');
+        var key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
 
-      var value = document.createElement('span');
-      value.innerHTML = layer;
-      item.appendChild(key);
-      item.appendChild(value);
-      legend.appendChild(item);
+        var value = document.createElement('span');
+        value.innerHTML = layer;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
     }
 });
 
 map.getCanvas().style.cursor = 'default';
 
-map.on('mousemove', function(e) {
-  var states = map.queryRenderedFeatures(e.point, {
-    layers: ['albay-cells']
-  });
+map.on('mousemove', "albay-cells", function(e) {
 
-  if (states.length > 0) {
-    document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.MUNICITY + '</strong></h3><p>ELEVATION: <strong>' + states[0].properties.Elevation + ' meters </strong></p>' +
-    '<p>Change in NDVI: <strong>' + states[0].properties.D_NDVI + '</strong></p>' +
-    '<p>Change in LST: <strong>' + states[0].properties.D_LST + ' ° C</strong></p>';
-  } else {
-    document.getElementById('pd').innerHTML = '<p>Hover over a cell!</p>';
-  }
+    map.setFilter("albay-cells-hover", ["==", "CID", e.features[0].properties.CID]);
+    // map.setFilter("albay-cells-3d-hover", ["==", "CID", e.features[0].properties.CID]);
+
+    var states = map.queryRenderedFeatures(e.point, {
+    layers: ['albay-cells']
+    });
+
+    if (states.length > 0) {
+        document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.MUNICITY + '</strong></h3><p>ELEVATION: <strong>' + states[0].properties.Elevation + ' meters </strong></p>' +
+        '<p>Change in NDVI: <strong>' + states[0].properties.D_NDVI + '</strong></p>' +
+        '<p>Change in LST: <strong>' + states[0].properties.D_LST + ' ° C</strong></p>';
+    } else {
+        document.getElementById('pd').innerHTML = '<p>Hover over a cell!</p>';
+    }
+    });
+
+map.on("mouseleave", "albay-cells", function() {
+    map.setFilter("albay-cells-hover", ["==", "name", ""]);
+    // map.setFilter("albay-cells-3d-hover", ["==", "name", ""]);
 });
 
 map.addControl(new mapboxgl.NavigationControl());
